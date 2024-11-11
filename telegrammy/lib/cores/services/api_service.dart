e8f4ab24da93163730ed3e5557f5bd29bc7eb585
Khater <mohamed.khater03@eng-st.cu.edu.eg>
2024-11-11 16:16:44 +0200

    Add unit testing

--- telegrammy/lib/cores/services/api_service.dart
+++ telegrammy/lib/cores/services/api_service.dart
@@ -1,284 +1,53 @@
 import 'dart:async';
 import 'package:dartz/dartz.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:github/github.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 import 'package:dio/dio.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:telegrammy/cores/constants/api_constants.dart';
 // import 'package:flutter_web_auth_plus/flutter_web_auth_plus.dart';
 import 'package:telegrammy/cores/services/service_locator.dart';
 import 'package:telegrammy/cores/services/token_storage_service.dart';
-import 'package:flutter_web_auth/flutter_web_auth.dart';
 
 class ApiService {
   ApiService({required this.dio});
-  final FirebaseAuth _auth = FirebaseAuth.instance;
-  GoogleSignIn _googleSignIn = GoogleSignIn(
-    scopes: [
-      'email',
-      'profile', // Access basic profile info
-    ],
-  );
   final Dio dio;
 
-  // Future<void> initUniLinks() async {
-  //   StreamSubscription sub = uriLinkStream.listen((Uri? uri) {
-  //     if (uri != null && uri.scheme == 'http://192.168.0.102:5173') {
-  //     }
-  //   });
-  // }
-
-// Function to launch Google Sign-In
-
-  // Future<void> signInWithGoogle() async {
-  //   final url = "http://192.168.0.102:8080/api/v1/auth/google";
-
-  //   try {
-  //     // Launch the URL and listen for the callback URL
-  //     final result = await FlutterWebAuth.authenticate(
-  //         url: url,
-  //         callbackUrlScheme:
-  //             "myapp" // match the scheme in the URL returned by backend
-  //         );
-
-  //     // Extract the token from the result
-  //     final token = Uri.parse(result).queryParameters['token'];
-
-  //     if (token != null) {
-  //       // Store the token and navigate in the app as needed
-  //       print(token);
-  //       // await saveToken(token);
-  //       // navigateToHomeScreen();
-  //     }
-  //   } catch (e) {
-  //     print("Error: $e");
-  //   }
-  // }
-
-  // Future<void> signInWithGoogle() async {
-  //   final url = "http://backtest.telegrammy.tech:8080/api/v1/auth/google";
-
-  //   try {
-  //     // Launch the URL and listen for the callback URL
-  //     final result = await FlutterWebAuth.authenticate(
-  //         url: url,
-  //         callbackUrlScheme:
-  //             "myapp" // match the scheme in the URL returned by backend
-  //         );
-
-  //     // Extract the token from the result
-  //     final token = Uri.parse(result).queryParameters['token'];
-  //     print(token);
-  //     if (token != null) {
-  //       // Store the token and navigate in the app as needed
-  //       // await saveToken(token);
-  //       // navigateToHomeScreen();
-  //     }
-  //   } catch (e) {
-  //     print("Error: $e");
-  //   }
-  // }
-
-  // Future<void> signInWithGoogle() async {
-  //   // initUniLinks();
-  //   const url = 'http://192.168.0.102:8080/api/v1/auth/google';
-
-  //   try {
-  //     final Uri uri = Uri.parse(url); // Create a Uri object from the string
-  //     await launchUrl(uri,
-  //         mode: LaunchMode
-  //             .externalApplication); // Use launchUrl with the Uri object
-  //   } catch (e) {
-  //     print(e);
-  //   }
-  // }
-  Future<void> signInWithGoogle() async {
+  Future<Either<String, void>> signInWithGoogle() async {
     try {
       final Uri url =
           Uri.parse('http://backtest.telegrammy.tech:8080/api/v1/auth/google');
-      // if (await canLaunchUrl(url)) {
-      await launchUrl(
-        url,
-        mode: LaunchMode.externalApplication, // Open in external browser
-      );
-      // } else {
-      //   throw 'Could not launch $url';
-      // }
+      if (await canLaunchUrl(url)) {
+        await launchUrl(
+          url,
+          mode: LaunchMode.externalApplication, // Open in external browser
+        );
+      } else {
+        return left('Could not launch $url');
+      }
 
-      print('success');
-      // final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
-      // if (googleUser == null) {
-      //   throw Exception('Sign-in aborted by user'); // User canceled the sign-in
-      // }
-      // print(googleUser);
-      // final GoogleSignInAuthentication? googleAuth =
-      //     await googleUser.authentication;
-      // print(googleAuth?.idToken);
-      // final String idToken = googleAuth!.idToken!;
-      // print(idToken);
-      // Send the ID token to the backend using Dio or another HTTP client.
-      // Example:
-      // final response = await dio.post(...);
-      // if (response.statusCode != 200) {
-      //   throw Exception('Sign-in failed: ${response.data['error']}');
-      // }
-
+      return Right(null);
       // If successful, do nothing and return void
     } catch (error) {
-      throw Exception('Sign-in error: $error'); // Propagate error
+      return left('Sign-in error: $error');
     }
   }
 
-  Future<void> signInWithFacebook() async {
-    const url = 'http://10.0.2.2:8080/api/v1/auth/github';
-
-    // const url = 'http://192.168.0.102:8080/api/v1/auth/github';
-
+  Future<Either<String, void>> signInWithGitHub() async {
     try {
-      final Uri uri = Uri.parse(url); // Create a Uri object from the string
-      print(uri);
-      await launchUrl(uri,
-          mode: LaunchMode
-              .externalApplication); // Use launchUrl with the Uri object
-    } catch (e) {
-      print(e);
+      final Uri url =
+          Uri.parse('http://backtest.telegrammy.tech:8080/api/v1/auth/github');
+      if (await canLaunchUrl(url)) {
+        await launchUrl(
+          url,
+          mode: LaunchMode.externalApplication, // Open in external browser
+        );
+      } else {
+        return left('Could not launch $url');
+      }
+      return Right(null);
+    } catch (error) {
+      return left('Sign-in error: $error');
     }
-    // // Trigger the sign-in flow
-    // final LoginResult loginResult = await FacebookAuth.instance.login();
-
-    // // Create a credential from the access token
-    // final OAuthCredential facebookAuthCredential =
-    //     FacebookAuthProvider.credential(loginResult.accessToken!.tokenString);
-
-    // // Once signed in, return the UserCredential
-    // return FirebaseAuth.instance.signInWithCredential(facebookAuthCredential);
-  }
-
-  Future<void> signInWithGitHub() async {
-    final Uri url =
-        Uri.parse('http://backtest.telegrammy.tech:8080/api/v1/auth/github');
-    // if (await canLaunchUrl(url)) {
-    await launchUrl(
-      url,
-      mode: LaunchMode.externalApplication, // Open in external browser
-    );
-    // } else {
-    //   throw 'Could not launch $url';
-    // }
-
-    print('success');
-    // final clientId = 'Ov23lickxMhmwQkCMM4W';
-    // final clientSecret = 'c2316813977f0abdbb00650f299a3925ced6272b';
-    //    const String url = "https://github.com/login/oauth/authorize" +
-    //       "?client_id=" + 'Ov23lickxMhmwQkCMM4W' +
-    //       "&scope=public_repo%20read:user%20user:email";
-    // if (await canLaunch(url)) {
-    //     await launch(
-    //       url,
-    //       forceSafariVC: false,
-    //       forceWebView: false,
-    //     );
-    //   } else {
-    //     print("CANNOT LAUNCH THIS URL!");
-    //   }
-
-    // GithubAuthProvider githubAuthProvider = GithubAuthProvider();
-    // UserCredential userCredential =
-    //     await FirebaseAuth.instance.signInWithProvider(githubAuthProvider);
-    // print(userCredential);
-    // return userCredential;
-    // final firebaseAuth = FirebaseAuth.instance;
-    // final dio = Dio();
-
-    // // GitHub OAuth URL
-    // final clientId = 'Ov23lickxMhmwQkCMM4W';
-    // final clientSecret = 'c2316813977f0abdbb00650f299a3925ced6272b';
-    // final redirectUrl =
-    //     'https://telegrammy-d0854.firebaseapp.com/__/auth/handler';
-
-    // // Step 1: Trigger the GitHub authentication process
-    // final result = await FlutterWebAuth.authenticate(
-    //   url:
-    //       'https://github.com/login/oauth/authorize?client_id=$clientId&redirect_uri=$redirectUrl',
-    //   callbackUrlScheme: 'https',
-    // );
-
-    // // Step 2: Extract the code from the callback URL
-    // final code = Uri.parse(result).queryParameters['code'];
-
-    // // Step 3: Exchange the code for an access token using Dio
-    // try {
-    //   final response = await dio.post(
-    //     'https://github.com/login/oauth/access_token',
-    //     data: {
-    //       'client_id': clientId,
-    //       'client_secret': clientSecret,
-    //       'code': code,
-    //     },
-    //     options: Options(
-    //       headers: {
-    //         'Accept': 'application/json',
-    //       },
-    //     ),
-    //   );
-
-    //   final accessToken = response.data['access_token'];
-
-    //   // Step 4: Use the GitHub access token to sign in with Firebase
-    //   final githubAuthCredential = GithubAuthProvider.credential(accessToken);
-
-    //   return await firebaseAuth.signInWithCredential(githubAuthCredential);
-    // } on DioException catch (e) {
-    //   print("Error during GitHub sign-in: ${e.response?.data}");
-    //   throw e;
-    // }
-
-//  final github = GitHub(
-//     clientId: 'YOUR_CLIENT_ID',
-//     clientSecret: 'YOUR_CLIENT_SECRET',
-//     redirectUrl: 'yourapp://auth',
-//   );
-
-//   try {
-//     final authUrl = github.getAuthorizationUrl();
-//     // Use a webview or browser to navigate to authUrl.
-//     // After user authenticates, GitHub will redirect to the callback with a code.
-
-//     // Retrieve the code from the redirect and exchange it for an access token.
-//     final token = await github.exchangeOAuthCodeForToken(code);
-//     // Use the token to make authenticated requests.
-//   } catch (error) {
-//     print('GitHub sign-in failed: $error');
-//   }
-
-    // final clientId = 'Ov23lickxMhmwQkCMM4W';
-    // // final redirectUri =
-    // //     'http://localhost:8000/auth/callback'; // Same as in GitHub settings
-    // final redirectUri = 'telegrammy://auth';
-
-    // final url = Uri.https('github.com', '/login/oauth/authorize', {
-    //   'client_id': clientId,
-    //   'redirect_uri': redirectUri,
-    //   'scope': 'read:user user:email',
-    // });
-    // print(url.toString());
-    // // Open the GitHub login page
-    // final result = await FlutterWebAuth.authenticate(
-    //     url: url.toString(), callbackUrlScheme: "telegrammy");
-
-    // // Extract the code from the result
-    // final code = Uri.parse(result).queryParameters['code'];
-    // if (code != null) {
-    //   // Exchange code for an access token
-    //   final accessToken = await getAccessToken(code);
-    //   if (accessToken != null) {
-    //     // Use accessToken to fetch user details
-    //     final userData = await getGitHubUser(accessToken);
-    //     print("User Info: $userData");
-    //   }
-    // }
   }
 
 // Function to exchange the authorization code for an access token using Dio
@@ -308,31 +77,6 @@ class ApiService {
     }
 
     return null;
-  }
-
-// Function to fetch user information from GitHub using Dio
-  Future<Map<String, dynamic>?> getGitHubUser(String accessToken) async {
-    try {
-      final dio = Dio();
-      final response = await dio.get(
-        'https://api.github.com/user',
-        options: Options(headers: {
-          'Authorization': 'token $accessToken',
-        }),
-      );
-
-      if (response.statusCode == 200) {
-        return response.data;
-      }
-    } catch (e) {
-      print("Error fetching user data: $e");
-    }
-
-    return null;
-  }
-
-  Future<void> signOutGoogle() async {
-    await _googleSignIn.signOut();
   }
 
   Future<void> signUpUser(Map<String, dynamic> userData) async {
--- telegrammy/lib/features/auth/presentation/widgets/customSigninButton.dart
+++ telegrammy/lib/features/auth/presentation/widgets/customSigninButton.dart
@@ -1,12 +1,5 @@
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:go_router/go_router.dart';
-import 'package:telegrammy/cores/routes/routes_name.dart';
-// import 'package:github_sign_in_plus/github_sign_in_plus.dart';
-import 'package:telegrammy/features/auth/presentation/view_models/login_cubit/login_cubit.dart';
-import 'package:uni_links/uni_links.dart';
 
 class Customsigninbutton extends StatefulWidget {
   final IconData icon;
@@ -19,58 +12,26 @@ class Customsigninbutton extends StatefulWidget {
 }
 
 class _CustomsigninbuttonState extends State<Customsigninbutton> {
-  StreamSubscription? _sub;
   @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    _handleIncomingLinks();
-  }
-
-  // Handle incoming links
-  void _handleIncomingLinks() {
-    _sub = uriLinkStream.listen((Uri? uri) {
-      if (uri != null) {
-        debugPrint('Received deep link: $uri');
-      }
-    }, onError: (err) {
-      debugPrint('Error: $err');
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return BlocListener<LoginCubit, LoginState>(
-      listener: (context, state) {
-        if (state is LoginSucess) {
-          // context.goNamed(RouteNames.home); // Navigate to home on success
-        } else if (state is LoginError) {
-          // ScaffoldMessenger.of(context).showSnackBar(
-          //   SnackBar(
-          //     content: Text(state.errorMessage), // Assuming state has errorMessage
-          //   ),
-          // );
-        }
-      },
-      child: Container(
-        width: 108,
-        height: 56,
-        decoration: BoxDecoration(
-          border: Border.all(
-            color: Colors.grey, // Border color
-            width: 1.0, // Border width
-          ),
-          borderRadius: BorderRadius.circular(8.0), // Rounded corners
+    return Container(
+      width: 108,
+      height: 56,
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey, // Border color
+          width: 1.0, // Border width
         ),
-        child: IconButton(
-          onPressed: () async {
-            // context.read<LoginCubit>().signinWithGoogleCubit(); // Trigger Google sign-in
-            widget.signinWithSocialAccount();
-          },
-          icon: Icon(
-            widget.icon,
-            size: 25.0, // Icon size
-          ),
+        borderRadius: BorderRadius.circular(8.0), // Rounded corners
+      ),
+      child: IconButton(
+        onPressed: () async {
+          widget.signinWithSocialAccount();
+        },
+        icon: Icon(
+          widget.icon,
+          size: 25.0, // Icon size
         ),
       ),
     );
